08d55128614d398336875a1b20f90c3555870069
svegagarrot <svegagarrot@itba.edu.ar>
2024-07-02 19:13:39 -0300

    añade funcion fillArr

--- infractionsADT.c
+++ infractionsADT.c
@@ -30,7 +30,7 @@ typedef struct agency{
 typedef struct agency *TListAgency;
 
 typedef struct id{
-    char exists;
+    size_t id;
     TListInfractions pNode;
 }TId;
 
@@ -50,14 +50,14 @@ typedef struct infractionSystemCDT{
     size_t *arrYears[MONTHS]; //vector de años y meses (multas)
     size_t minYear; //años para la query 4
     size_t maxYear;
-    TId arrId; // vector con los id ordenados
+    TId *arrId; // vector con los id ordenados
     size_t dim; //dimension total
     size_t size; //espacio ocupado
 }infractionSystemCDT;
 
 
 infractionSystemADT newInfractionSystem(size_t minYear, size_t maxYear){
-        infractionSystemADT newSystem = calloc(1,sizeof(infractionSystemADT));
+        infractionSystemADT newSystem = calloc(1,sizeof(infractionSystemCDT));
         errno = 0;
         //podriamos hacer una macro !!!  
         if(newSystem == NULL || errno == ENOMEM){
@@ -68,32 +68,51 @@ infractionSystemADT newInfractionSystem(size_t minYear, size_t maxYear){
         return newSystem;
 }
 
-static TListInfractions addInfractionRec(TListInfractions list,char *description,size_t id,size_t *added){
+static int cmpIds(const void *a, const void *b){
+    const TId *idA = (const TId *)a;
+    const TId *idB = (const TId *)b;
+    return (idA->id - idB->id);
+}
+
+
+static TId *fillArr(TId *array, size_t dim, TListInfractions pInfraction){
+    array  = realloc(array,sizeof(*array) * dim);
+    //chequear realloc
+    array[dim-1].pNode = pInfraction;
+    array[dim-1].id = pInfraction->id;
+    qsort(array,dim,sizeof(TId),cmpIds);
+    return array;
+}
+
+static TListInfractions addInfractionRec(TListInfractions list,char *description,size_t id,size_t *added,TListInfractions *pInfractions){
     int c;
     if(list == NULL || (c = strcasecmp(list->description,description)) > 0){
         TListInfractions newNode = malloc(sizeof(TInfractions));
         newNode->description = malloc(strlen(description) + 1);
         //Verificar que los malloc hayan funcionado
-        strcpy(newNode->description,description);
+        strcpy(newNode->description,description); //chequear esto
         newNode->firstTicket = NULL;
         newNode->tail = list;
         newNode->totalFines = 0;
         newNode->id = id;
         *added = 1;
+        *pInfractions = newNode;
         return newNode;
     }
     else if(c < 0){
-        list->tail = addInfractionRec(list->tail,description,id,added);
+        list->tail = addInfractionRec(list->tail,description,id,added,pInfractions);
     }
     return list;
 }
 
 int addInfraction(infractionSystemADT infractionSystem,char *description,size_t id){
     size_t added = 0; // flag para saber si agrego la infraccion
-    infractionSystem->firstInfraction = addInfractionRec(infractionSystem->firstInfraction,description,id,&added);
+    TListInfractions pInfractions; // variable con los datos del nodo agregado
+    infractionSystem->firstInfraction = addInfractionRec(infractionSystem->firstInfraction,description,id,&added,&pInfractions);
 
     if(added){
-        //agregar el id al vector, agrandarlo y ordenarlo
+        infractionSystem->dim++;
+        infractionSystem->arrId = fillArr(infractionSystem->arrId,infractionSystem->dim,pInfractions);
     }
 
     return added;
